bd52becccb2bb87ca31a0d27dbb1f04e360d666a
CrossTech <526383173@qq.com>
2022-05-03 21:58:55 +0800

    Added color schemes into themes

--- app/src/main/java/crosstech/future/MainActivity.kt
+++ app/src/main/java/crosstech/future/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
+        // initialization
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         val navigationBar = binding.navigationView
