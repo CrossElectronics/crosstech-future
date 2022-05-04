84d97b09ebc66fdc7df4f0adc170760de0b0ad9b
CrossTech <526383173@qq.com>
2022-05-04 16:24:00 +0800

    Changed fonts

--- app/src/main/java/crosstech/future/DashboardFragment.kt
+++ app/src/main/java/crosstech/future/DashboardFragment.kt
@@ -1,9 +1,28 @@
 package crosstech.future
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import crosstech.future.databinding.ActivityMainBinding
+import crosstech.future.databinding.DashboardFragmentBinding
 
 class DashboardFragment: Fragment(R.layout.dashboard_fragment)
 {
-
+    private lateinit var binding: DashboardFragmentBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View
+    {
+        // initialization
+        binding = DashboardFragmentBinding.inflate(inflater)
+        binding.textView3.typeface = resources.getFont(R.font.nova_mono)
+        binding.textView3.text = "--:--:--"
+        return binding.root
+    }
 }
