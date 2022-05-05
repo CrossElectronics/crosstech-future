7f42960b3d166cb1a7c37d7e8204b5310169346c
CrossTech <526383173@qq.com>
2022-05-05 13:29:13 +0800

    Implemented custom serializer for LocalDateTime

---
+++ app/src/main/java/crosstech/future/logics/models/serializers/LocalDateTimeAsStringSerializer.kt
@@ -0,0 +1,28 @@
+package crosstech.future.logics.models.serializers
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.time.DateTimeException
+import java.time.LocalDateTime
+import java.util.*
+
+object LocalDateTimeAsStringSerializer : KSerializer<LocalDateTime>
+{
+    override fun deserialize(decoder: Decoder): LocalDateTime
+    {
+        val string = decoder.decodeString()
+        return LocalDateTime.parse(string)
+    }
+
+    override val descriptor: SerialDescriptor
+        get() = PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: LocalDateTime)
+    {
+        encoder.encodeString(value.toString())
+    }
+}
