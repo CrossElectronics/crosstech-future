ebedf5eb7b1bdeae4beaca239a33b65d1cb6f3cd
CrossTech <526383173@qq.com>
2022-05-05 13:48:03 +0800

    Fixed bug where Serialization is unable to process lateinit vars. Resorted to nullable. Serialization unit test passed.

--- app/src/test/java/crosstech/future/ExampleUnitTest.kt
+++ app/src/test/java/crosstech/future/ExampleUnitTest.kt
@@ -1,8 +1,16 @@
 package crosstech.future
 
+import crosstech.future.logics.enums.Urgency
+import crosstech.future.logics.models.Task
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromHexString
+import kotlinx.serialization.encodeToByteArray
+import kotlinx.serialization.encodeToHexString
+import kotlinx.serialization.protobuf.ProtoBuf
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.time.LocalDateTime
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,8 +20,20 @@ import org.junit.Assert.*
 class ExampleUnitTest
 {
     @Test
-    fun addition_isCorrect()
+    fun taskSerialization()
     {
-        assertEquals(4, 2 + 2)
+        val task = Task(
+            "Dummy task",
+            "lorem ipsum of the day",
+            LocalDateTime.now(),
+            Urgency.Normal,
+            5
+        )
+        val serialized = ProtoBuf.encodeToHexString(task)
+        val size = ProtoBuf.encodeToByteArray(task).size
+        println("Serialized to $size Bytes")
+        println(serialized)
+        val deserialized = ProtoBuf.decodeFromHexString<Task>(serialized)
+        assert(task == deserialized)
     }
 }
