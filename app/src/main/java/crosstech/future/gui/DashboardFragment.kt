21cfcab27b78f47fba6df4aea1d7b9e1c984e5cd
CrossTech <526383173@qq.com>
2022-05-16 17:46:24 +0800

    Implemented showing file size on dashboard

--- app/src/main/java/crosstech/future/gui/DashboardFragment.kt
+++ app/src/main/java/crosstech/future/gui/DashboardFragment.kt
@@ -1,16 +1,22 @@
 package crosstech.future.gui
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import crosstech.future.Global
 import crosstech.future.R
 import crosstech.future.databinding.DashboardFragmentBinding
+import crosstech.future.logics.Utils.Companion.getSaveSize
+import crosstech.future.logics.Utils.Companion.toReadable
 
 class DashboardFragment: Fragment(R.layout.dashboard_fragment)
 {
     private lateinit var binding: DashboardFragmentBinding
+    private lateinit var global: Global
+    private lateinit var activity: Activity
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -19,9 +25,29 @@ class DashboardFragment: Fragment(R.layout.dashboard_fragment)
     {
         // initialization
         binding = DashboardFragmentBinding.inflate(inflater)
-
+        activity = requireActivity()
+        global = activity.applicationContext as Global
         return binding.root
     }
     // TODO: Implement dashboard
     // Features: Ongoing devotion, scheduled today, next deadline, stats
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+        updateSize()
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?)
+    {
+        super.onViewStateRestored(savedInstanceState)
+        updateSize()
+    }
+
+    private fun updateSize()
+    {
+        val size =
+            Global.TASKS_FILE.getSaveSize(activity) + Global.ARCHIVE_FILE.getSaveSize(activity)
+        binding.tvTotalUsage.text = size.toReadable()
+    }
 }
--- app/src/main/java/crosstech/future/logics/Utils.kt
+++ app/src/main/java/crosstech/future/logics/Utils.kt
@@ -1,5 +1,7 @@
 package crosstech.future.logics
 
+import android.content.Context
+import java.io.File
 import java.security.MessageDigest
 
 class Utils
@@ -13,6 +15,22 @@ class Utils
         {
             val digest = MessageDigest.getInstance("SHA1")
             return digest.digest(this.toByteArray()).toHex()
+        }
+
+        fun String.getSaveSize(context: Context): Long
+        {
+            val file = File(context.filesDir.absolutePath + "/" + this)
+            return if (file.exists()) file.length() else 0
+        }
+
+        fun Long.toReadable(): String
+        {
+            return when
+            {
+                this > 1.2 * 1024 * 1024 -> "${String.format("%2f", this / 1024.0 / 1024.0)} MB"
+                this > 1.2 * 1024        -> "${String.format("%2f", this / 1024.0)} KB"
+                else                     -> "$this B"
+            }
         }
     }
 }
